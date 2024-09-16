094aa56e4df6e07f02a462e0a5de40ec7ed2f18c
Manita Paudel <paudel04manita@gmail.com>
2024-09-16 19:40:53 +0545

    Create api route for DELETE

---
+++ src/app/api/tickets/[id]/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import Ticket from "@/app/(models)/Ticket";
+
+export async function DELETE(req: any, { params }: any) {
+  try {
+    const { id } = params;
+
+    await Ticket.findByIdAndDelete(id);
+    NextResponse.json({ message: "Ticket Deleted" }, { status: 200 });
+  } catch (error) {
+    NextResponse.json(
+      {
+        message: "Error",
+      },
+      { status: 500 }
+    );
+  }
+}
